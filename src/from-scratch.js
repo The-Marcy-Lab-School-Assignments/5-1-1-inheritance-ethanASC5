52fd637889b8aff8dc68d2900145cee970c72af6
Marcy <marcy@Marcys-MBP-4.home>
2024-08-13 01:44:03 -0400

    finished

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,14 +1,44 @@
 class Quadrilateral {
+  constructor(side1, side2, side3, side4) {
+    this.side1 = side1
+    this.side2 = side2
+    this.side3 = side3
+    this.side4 = side4
+  }
+  getPerimeter() {
+    return this.side1 + this.side2 + this.side3 + this.side4
+  }
 
 }
 
-class Rectangle {
+class Rectangle extends Quadrilateral{
+  constructor(side1, side2) {
+    super(side1, side2, side1, side2)
+  }
+  getArea() {
+    return this.side1 * this.side2
+  }
+
+  getDiagonal() {
+    return Math.sqrt(this.side1 ** 2 + this.side2 ** 2)
+  }
+}
+
+class Square extends Rectangle {
+  constructor(side){
+    super(side, side);
+    this.side = side;
+  }
+
+
+  getDiagonal() {
+    return Math.sqrt(2) * this.side;
+}
+
+
 
 }
 
-class Square {
-
-}
 
 /* Be creative with this one! */
 class Person {
